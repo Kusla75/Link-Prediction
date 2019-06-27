bc006fa8a27081f5817de7d67bdd3f907589f00b
Kusla75 <nikola.kuslakovic1616@gmail.com>
2019-06-27 22:07:25 +0200

    Dodate su sve ostale metrike: - Jacardov koeficijent - Preferential attachment - Adamic/Adar metrika - Resource allocation

--- metric_functions.py
+++ metric_functions.py
@@ -1,4 +1,6 @@
 from math import log10
+import igraph as ig
+
 
 def intersection(list1, list2):
     inter_list = []
@@ -7,3 +9,81 @@ def intersection(list1, list2):
             inter_list.append(value)
     return inter_list
 
+
+def common_neighbors(graph, n1, n2):
+    ngh1 = graph.neighbors(n1).sort()
+    ngh2 = graph.neighbors(n2).sort()
+
+    if n1 in ngh2:                      # Pošto veza postoji onda su nod 1 i nod 2 međusobno povezani i
+        ngh1.remove(n2)                 # moraju se ukloniti iz međusobnih lista komšija, jer merimo vrednost
+        ngh2.remove(n1)                 # za međusobne veze koje ne postoje a mogle bi (pravimo se kao da veza 
+                                        # između noda 1 i noda 2 ne postoji)
+    return intersection(ngh1, ngh2)
+
+
+def jaccards_coefficient(graph, n1, n2):
+    ngh1 = graph.neighbors(n1).sort()
+    ngh2 = graph.neighbors(n2).sort()
+
+    if n1 in ngh2:   
+        ngh1.remove(n2)                 
+        ngh2.remove(n1)
+
+    try:
+        score = len(intersection(ngh1, ngh2))/len(ngh1 + ngh2)
+    except ZeroDivisionError:
+        score = 0
+
+    return score 
+
+
+def preferential_attachment(graph, n1, n2):
+    ngh1 = graph.neighbors(n1).sort()
+    ngh2 = graph.neighbors(n2).sort()
+
+    if n1 in ngh2:
+        ngh1.remove(n2)
+        ngh2.remove(n1)
+
+    return len(ngh1) * len(ngh2)
+
+
+def adamic_adar(graph, n1, n2):
+    ngh1 = graph.neighbors(n1).sort()
+    ngh2 = graph.neighbors(n2).sort()
+
+    if n2 in ngh1:
+        ngh1.remove(n2)
+        ngh2.remove(n1)
+
+    inter_list = intersection(ngh1, ngh2)
+
+    for node in inter_list:
+        try:
+            sum += 1/log10(len(list(graph.neighbors(node))))
+        except ZeroDivisionError:
+            continue
+
+    return sum
+
+
+def resource_allocation(graph, n1, n2):
+    sum = 0
+    ngh1 = list(graph.neighbors(n1))
+    ngh2 = list(graph.neighbors(n2))
+    ngh1.sort()
+    ngh2.sort()
+
+    if n2 in ngh1:
+        ngh1.remove(n2)
+        ngh2.remove(n1)
+
+    inter_list = intersection(ngh1, ngh2)
+
+    for node in inter_list:
+        try:
+            sum += 1/len(list(graph.neighbors(node)))
+        except ZeroDivisionError:
+            continue
+
+    return sum
