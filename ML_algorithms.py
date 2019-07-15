02babec224e7fc5883625765904a30508419843d
Nikola Kuslakovic <nikola.kuslakovic1616@gmail.com>
2019-07-15 16:23:42 +0200

    Izmenjena je funkcija LogisticReg
    - Sada se umesto LogisticRegression koristi klasa LogisticRegressionCV.
      Ova izmena je uradjena kako bi se dobili koeficijenti feature-a
      od klasifikator

--- ML_algorithms.py
+++ ML_algorithms.py
@@ -6,7 +6,7 @@ import numpy as np
 import sklearn as skl
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegressionCV
 
 def LogisticReg(dataframe, label, cv_split, feat_to_drop = []):
     '''Funkcija koja poziva logisticku regresiju. Prvi argument je sam
@@ -22,8 +22,10 @@ def LogisticReg(dataframe, label, cv_split, feat_to_drop = []):
 
     x = np.array(dataframe.drop([label], 1))
 
-    logmodel = LogisticRegression()
+    logmodel = LogisticRegressionCV(cv = 5, random_state = 1, n_jobs = -1).fit(x,y)
 
-    average_score = cross_val_score(logmodel, x, y, cv = cv_split).mean()
-    
+#    average_score = cross_val_score(logmodel, x, y, cv = cv_split, n_jobs = -1).mean()
+ 
+    average_score = logmodel.score(x,y)
+    print(logmodel.coef_)
     return average_score
